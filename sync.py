fabd35274784044d297b830d91ea783372d2fc1c
刘硕 <liushuo@zetyun.com>
2024-06-14 09:32:32 +0800

    sync

---
+++ sync.py
@@ -0,0 +1,34 @@
+import json
+import os
+import subprocess
+
+trans = [
+    'docker.io/labring/kubernetes-docker',
+    'docker.io/labring/kubernetes',
+    'docker.io/labring/helm',
+    'docker.io/labring/flannel',
+    'docker.io/labring/calico',
+    'docker.io/library/ubuntu',
+    'docker.io/library/centos',
+]
+
+
+def get_tags(rep):
+    data = set()
+    try:
+        data = set(json.loads(subprocess.getoutput(f"skopeo list-tags docker://{rep}"))['Tags'])
+    except Exception:
+        pass
+    print(rep, data)
+    return data
+
+
+base = 'registry.cn-hangzhou.aliyuncs.com/acejilam'
+
+for item in trans:
+    base_image = item.split('/')[-1]
+    for tag in get_tags(item) - get_tags(base + '/' + base_image):
+        cmd = f'skopeo copy --all --insecure-policy docker://{item}:{tag} docker://registry.cn-hangzhou.aliyuncs.com/acejilam/{base_image}:{tag}'
+        print(cmd)
+        os.system(cmd)
+    # os.system(f'skopeo sync --src docker --dest docker {item.f} {item.t} --src-tls-verify=false --dest-tls-verify=false')
