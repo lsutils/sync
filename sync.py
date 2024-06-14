03c174c80e3b61004a7437282d42ad2a816a52d4
刘硕 <liushuo@zetyun.com>
2024-06-14 10:29:21 +0800

    sync

--- sync.py
+++ sync.py
@@ -27,8 +27,11 @@ base = 'registry.cn-hangzhou.aliyuncs.com/acejilam'
 
 for item in trans:
     base_image = item.split('/')[-1]
-    for tag in get_tags(item) - get_tags(base + '/' + base_image):
+    data = list(get_tags(item) - get_tags(base + '/' + base_image))
+    i = 0
+    for tag in sorted(data):
         cmd = f'skopeo copy --all --insecure-policy docker://{item}:{tag} docker://registry.cn-hangzhou.aliyuncs.com/acejilam/{base_image}:{tag}'
-        print(cmd)
+        print(i, "/", len(data), cmd, flush=True)
         os.system(cmd)
+        i += 1
     # os.system(f'skopeo sync --src docker --dest docker {item.f} {item.t} --src-tls-verify=false --dest-tls-verify=false')
