6f9a6febdbfd66ac1a6ef46517db356ef10e4f03
acejilam <acejilam@gmail.com>
2025-03-03 14:23:31 +0800

    init
    Signed-off-by: acejilam <acejilam@gmail.com>

--- sync.py
+++ sync.py
@@ -4,11 +4,11 @@ import random
 import subprocess
 import sys
 
+# data = list(range(2, 10))
+# data.append('')
+# seq = random.sample(list(data), k=1)[0]
+# os.system(f"skopeo login -u pulldownload{seq}@gmail.com docker.io -p pulldownload{seq}")
 
-data = list(range(2, 10))
-data.append('')
-seq = random.sample(list(data), k=1)[0]
-os.system(f"skopeo login -u pulldownload{seq}@gmail.com docker.io -p pulldownload{seq}")
 
 def get_tags(rep):
     data = set()
