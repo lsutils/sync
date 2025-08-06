6c42288ab294ebd1a7f54ce1651c27ffbd5c3772
acejilam <acejilam@gmail.com>
2025-08-06 17:45:39 +0800

    sync
    Signed-off-by: acejilam <acejilam@gmail.com>

--- local_run.py
+++ local_run.py
@@ -1,9 +1,10 @@
+import json
+import random
 import os
-import yaml
 
-res = yaml.safe_load(open('.github/workflows/sync.yaml', 'r', encoding='utf8'))
-
-raw = res['jobs']['build']['strategy']['matrix']['syncs']
-for item in raw:
-    # REDIS_HOST={REDIS_HOST} REDIS_PASSWORD={REDIS_PASSWORD} python3 local_run.py
-    os.system(f'python3 sync.py {item} -v')
+with open('tasks.json', 'r', encoding='utf8') as f:
+    data= json.loads(f.read())
+    random.shuffle(data)
+    for item in data:
+        # REDIS_HOST={REDIS_HOST} REDIS_PASSWORD={REDIS_PASSWORD} python3 local_run.py
+        os.system(f'python3 sync.py {item} -v')
