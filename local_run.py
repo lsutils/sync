a1f1de8a02cf648640460aeaf005b56ada9f34aa
acejilam <acejilam@gmail.com>
2026-01-14 23:56:23 +0800

    -
    Signed-off-by: acejilam <acejilam@gmail.com>

--- local_run.py
+++ local_run.py
@@ -5,6 +5,7 @@ import os
 with open('random-tasks.json', 'r', encoding='utf8') as f:
     data = list(json.loads(f.read()).keys())
     random.shuffle(data)
-    for item in data:
+    for i, item in enumerate(data):
+        print(f'[{i}/{len(data)}] {item}')
         # REDIS_HOST={REDIS_HOST} REDIS_PASSWORD={REDIS_PASSWORD} python3 local_run.py
         os.system(f'python3 sync.py {item} -v')
