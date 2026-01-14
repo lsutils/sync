d111baaabcc966197a1a80c968ca913b85bd1d2a
acejilam <acejilam@gmail.com>
2026-01-14 16:44:34 +0800

    docker.io/volcanosh/vc-agent-scheduler:latest

--- skopeo_copy.py
+++ skopeo_copy.py
@@ -23,7 +23,7 @@ with open(sync_path, 'r', encoding='utf8') as f:
 
 if __name__ == '__main__':
     repo_tag = sys.argv[1].strip()
-    if len(repo_tag.split('/')) < 2:
+    if len(repo_tag.split('/')) < 3:
         raise Exception('must have group/user/repo')
     repo, tag = '', ''
     if len(repo_tag.split(':')) == 2:
